2f4229b6f06f3eac58526c8bbed9b8a28c670c4b
Ivan Novosad <ivan.novosad@gmail.com>
2016-03-01 16:14:59 +0100

    Fix - Import all the images from bower assets directory to build/assets/images

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem "middleman", "~>4.0.0"
+gem "middleman", "~>4.1.2"
 gem "middleman-sprockets", "~>4.0.0.rc"
 gem "middleman-autoprefixer"
 gem "middleman-livereload"
--- config.rb
+++ config.rb
@@ -2,11 +2,20 @@
 config[:css_dir] = 'assets/stylesheets'
 config[:js_dir] = 'assets/javascripts'
 config[:images_dir] = 'assets/images'
+config[:images_extensions] = %w( svg jpg jpeg gif png webp )
 
 # Pull in assets installed from Bower
 @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
 bower_assets_dir = @bower_config["directory"]
 sprockets.append_path File.join app.root, bower_assets_dir
+
+masks = config[:images_extensions].map { |ext| "#{bower_assets_dir}/**/*.#{ext}" }
+
+# Import all the images from bower assets directory to build/assets/images
+Dir[*masks].each do |file_path|
+  relative_path = file_path[("#{bower_assets_dir}/".length)..-1] # e.g.: "open-iconic/png/resize-width.png"
+  import_file(file_path, "assets/images/#{relative_path}")
+end
 
 
 # Use pretty urls `www.example.com/blog`
